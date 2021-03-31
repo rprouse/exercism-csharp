81abdecb71501305fe37ffe1c0f3db5f8e60bf8f
Rob Prouse <rob@prouse.org>
2021-03-31 10:47:36 -0400

    Update gigasecond

---
+++ gigasecond/Gigasecond.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Gigasecond
+{
+    const double GIGASECOND = 1E9;
+
+    /// <summary>
+    /// Calculates the date that someone turned or will celebrate their 1 Gs anniversary.
+    /// </summary>
+    /// <returns></returns>
+    public static DateTime Add(DateTime birthday) =>
+        birthday.AddSeconds( GIGASECOND );
+}
