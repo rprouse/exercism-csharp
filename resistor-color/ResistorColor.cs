daa6730b1e3bed26a90e6493c0c0130e6f20a853
Rob Prouse <rob@prouse.org>
2021-03-31 13:36:48 -0400

    Solve resistor color

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    static readonly string[] colors = new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+    public static int ColorCode(string color) =>
+        Array.IndexOf(colors, color);
+
+    public static string[] Colors() => colors;
+}
