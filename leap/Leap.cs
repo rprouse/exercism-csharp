5d02ee7da017d0e7b426f94f2d287147a0fc56eb
Rob Prouse <rob@prouse.org>
2021-03-31 10:40:53 -0400

    Update leap year

---
+++ leap/Leap.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// Utility methods for working with years
+/// </summary>
+public static class Leap
+{
+    /// <summary>
+    /// Determines if the given year is a leap year
+    /// </summary>
+    /// <param name="year">The year in question</param>
+    /// <returns>True if the given year is a leap year</returns>
+    public static bool IsLeapYear(uint year)
+    {
+        if (year % 400 == 0) return true;
+        if (year % 100 == 0) return false;
+        return (year % 4 == 0);
+    }
+}
